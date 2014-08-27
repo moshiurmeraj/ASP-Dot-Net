c8930ad97bf823e732c7cec47bbc22ae438cc5bc
moshiurmeraj <merajlovesall@gmail.com>
2014-08-27 16:43:05 -0700

    new project added

---
+++ Day 29. 26-August-2014/PartialViewPassingModel/PartialViewPassingModel/PartialViewPassingModel/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartialViewPassingModel.Models;
+
+namespace PartialViewPassingModel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            Product product1 = new Product() { Name = "Pizza", Price = 1000 };
+            Product product2 = new Product() { Name = "Burger", Price = 250 };
+            Product product3 = new Product() { Name = "Fried Rice", Price = 300 };
+
+            List<Product> productList = new List<Product>() { product1, product2, product3 };
+            return View(productList);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Day 29. 26-August-2014/PartialViewPassingModel/PartialViewPassingModel/PartialViewPassingModel/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartialViewPassingModel.Models;
+
+namespace PartialViewPassingModel.Controllers
+{
+    public class ProductController: Controller
+    {
+        private ProductContext db = new ProductContext();
+
+        public ActionResult Index()
+
+        {
+            return View(db.Products.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return View(product);
+        }
+    }
+}
